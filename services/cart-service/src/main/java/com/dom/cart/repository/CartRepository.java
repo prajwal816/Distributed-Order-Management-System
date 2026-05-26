9095e02df48c4baec4c4074536ce2d575dfbce91
prajwal816 <prajwal1084@gmail.com>
2026-05-26 11:00:45 +0530

    feat: add CartRepository interface with findByUserId support

---
+++ services/cart-service/src/main/java/com/dom/cart/repository/CartRepository.java
@@ -0,0 +1,12 @@
+package com.dom.cart.repository;
+
+import com.dom.cart.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    Optional<Cart> findByUserId(Long userId);
+}
